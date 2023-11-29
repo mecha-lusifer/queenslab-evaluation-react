3b25d1275d4ca7da19e07f9813bdbf5acc5ab0ab
Luis Fernandez <luis.f.fernandez@outlook.com>
2023-11-29 10:20:28 +0100

    Small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from "react"
 import CreditCard from "./credit-card/CreditCard"
 
 const App = () => {
-  const [cardNumber, setCardNumber] = useState("");
-  const [cardHolder, setCardHolder] = useState("");
-  const [expirationMonth, setExpirationMonth] = useState("");
-  const [expirationYear, setExpirationYear] = useState("");
-  const [cvc, setCvc] = useState("");
-
   return (
     <div className="App">
-      <CreditCard onSubmit={(c) => alert(JSON.stringify(c))} />
+      <CreditCard onSubmit={(c) => fetch(
+        '/api/payment',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(c)
+        }
+      )}
+      />
     </div>
   )
 }
